34746112951272c7b1a62c50fb01a43fbd3b630f
Daffeinatek <samsquanchhunter14@gmail.com>
2016-02-22 15:49:07 -0600

    css update

--- source.js
+++ source.js
@@ -1,15 +1,24 @@
+var currTime = 0;
+var befTime = 0;
+
 function init(userName){
-getLastListened(userName);
+	currTime = Math.floor(Date.now() / 1000);
+	befTime = currTime - 43200; //last 12 hours
+	
+	getRecentList(userName, currTime, befTime);
 }
-function getLastListened(user){
- $.getJSON("https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=" + user + "&api_key=your api key&limit=3&format=json&callback=?", function(data) {
- var lastAlbumArt = $($(data.recenttracks.track).get(0).image).get(0)['#text'];
- var lastArtist = $(data.recenttracks.track).get(0).artist['#text'];
- var lastName = $(data.recenttracks.track).get(0).name;  	 
- var prevArtist = $(data.recenttracks.track).get(1).artist['#text'];
- var prevName = $(data.recenttracks.track).get(1).name;   	
-    //alert(lastAlbumArt);
-    document.getElementById("albumArt").src = lastAlbumArt;
-    document.getElementById("songInfo").innerHTML = "<b>" + lastName + "</b>" + "  by " + "<b>" + lastArtist + "</b>" + "<div id='prevSong'>" + "<b>" + prevName + "</b>" + "  by " + "<b>" + prevArtist + "</b>" + "</div>";    
-});
+
+
+function getRecentList(user, to, from){
+	$.getJSON("https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=" + user + "&api_key=yourapikeyhere&limit=200&to=" + to + "&from=" + from + "&format=json&callback=?", function(data) {
+		var lastAlbumArt = $($(data.recenttracks.track).get(0).image).get(0)['#text'];
+		var lastArtist = $(data.recenttracks.track).get(0).artist['#text'];
+		var lastName = $(data.recenttracks.track).get(0).name;  	 
+		var prevArtist = $(data.recenttracks.track).get(1).artist['#text'];
+		var prevName = $(data.recenttracks.track).get(1).name;   			
+		
+		document.getElementById("lastTwelve").innerHTML = "<b>" + Object.keys(data.recenttracks.track).length + "</b> in the last 12 hours";
+		document.getElementById("albumArt").src = lastAlbumArt;
+		document.getElementById("songInfo").innerHTML = "<div id='specBg'><b>" + lastName + "</b>" + "  by " + "<b>" + lastArtist + "</b>" + "<div id='prevSong'>" + "<b>" + prevName + "</b>" + "  by " + "<b>" + prevArtist + "</b>" + "</div></div>";    
+	});
 }
