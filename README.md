c9110bbdb826feb3c4dc58c3848de1ae836f2e93
Daffeinatek <samsquanchhunter14@gmail.com>
2016-02-22 13:39:59 -0600

    'Update'

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ![Alt text](https://github.com/HulaSamsquanch/lastfm-signature/blob/master/lastfm-signature.png?raw=true "last.fm signature")
 
 once you've hosted the html (and js), use an iframe to display it
+
+	<iframe src="link" width="600px" height="34px" frameBorder="0"></iframe>
--- source.js
+++ source.js
@@ -1,9 +1,8 @@
-function init(){
-var userName = "user goes here";
+function init(userName){
 getLastListened(userName);
 }
 function getLastListened(user){
- $.getJSON("https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=" + user + "&api_key=83a068985f1c01197759735f6cfaca92&limit=3&format=json&callback=?", function(data) {
+ $.getJSON("https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=" + user + "&api_key=your api key&limit=3&format=json&callback=?", function(data) {
  var lastAlbumArt = $($(data.recenttracks.track).get(0).image).get(0)['#text'];
  var lastArtist = $(data.recenttracks.track).get(0).artist['#text'];
  var lastName = $(data.recenttracks.track).get(0).name;  	 
