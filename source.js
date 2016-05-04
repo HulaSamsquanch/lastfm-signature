804f36a50abf4e244fd56cd0eca131050decacd4
Eli Nordstrom <samsquanchhunter14@gmail.com>
2016-05-03 22:36:03 -0500

    title and artist string trimming

--- source.js
+++ source.js
@@ -7,6 +7,34 @@ function init(userName){
 	done = getRecentList(userName, currTime, befTime);	
 }
 
+/*
+ * 18 per string
+ fuckin experimental
+ */
+function trimString(inStringOne, inStringTwo) {
+	var einStringOne = inStringOne;
+	var einStringTwo = inStringTwo;
+
+	var editDepth = false;
+	if(inStringOne.length + inStringTwo.length < 36){
+		return inStringOne + "</b>" + "  by " + "<b>" + inStringTwo;
+	}else{
+		while(einStringOne.length + inStringTwo.length > 36) {
+			if(einStringOne.length > 6) {
+				einStringOne = einStringOne.slice(0, -1);
+			}else{
+				einStringTwo = einStringTwo.slice(0, -1);
+			}
+			
+			//einStringTwo = einStringTwo.slice(0, -1);
+		}
+		if(!editDepth)
+			return einStringOne + "&hellip;</b>" + "  by " + "<b>" + inStringTwo;
+		else
+			return einStringOne + "&hellip;</b>" + "  by " + "<b>" + inStringTwo + "&hellip;";
+	}
+}
+
 function getRecentList(user, to, from){
 	$.getJSON("https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=" + user + "&api_key=yourapikey&limit=200&to=" + to + "&from=" + from + "&format=json&callback=?", function(data) {	
 		var lastAlbumArt = $($(data.recenttracks.track).get(0).image).get(0)['#text'];
@@ -16,23 +44,25 @@ function getRecentList(user, to, from){
 		var prevName = $(data.recenttracks.track).get(1).name;   	
 		var recentAmt = Object.keys(data.recenttracks.track).length;
 			
-		$.getJSON("https://ws.audioscrobbler.com/2.0/?method=track.getInfo&user=" + user + "&api_key=yourapikey&artist=" + lastArtist + "&track=" + lastName + "&format=json&callback=?", function(data) {
+		if (lastArtist && lastName) {
+			$.getJSON("https://ws.audioscrobbler.com/2.0/?method=track.getInfo&user=" + user + "&api_key=yourapikey&artist=" + lastArtist + "&track=" + lastName + "&format=json&callback=?", function(data) {
 			console.log(lastArtist+ " " + lastName+ " " + data.track.userloved);
 			if(data.track.userloved == "1"){
-				document.getElementById("mostRecent").innerHTML = "<b><font color='#990303'>&#10084;</font>   " + lastName + "</b>" + "  by " + "<b>" + lastArtist + "</b>";
+				document.getElementById("mostRecent").innerHTML = "<b><font color='#990303'>&#10084;</font>   " + trimString(lastName, lastArtist) + "</b>";
 			}else{
-				document.getElementById("mostRecent").innerHTML = "<b>" + lastName + "</b>" + "  by " + "<b>" + lastArtist + "</b>";
+				document.getElementById("mostRecent").innerHTML = "<b>" + trimString(lastName, lastArtist) + "</b>";
 			}
 			$.getJSON("https://ws.audioscrobbler.com/2.0/?method=track.getInfo&user=" + user + "&api_key=yourapikey&artist=" + prevArtist + "&track=" + prevName + "&format=json&callback=?", function(data) {
 				console.log(prevArtist+ " " + prevName+ " " + data.track.userloved);
 				if(data.track.userloved == "1"){
-					document.getElementById("previous").innerHTML = "<b><font color='#9B0505'>&#10084;</font>   " + prevName + "</b>" + "  by " + "<b>" + prevArtist + "</b>";
+					document.getElementById("previous").innerHTML = "<b><font color='#9B0505'>&#10084;</font>   " + trimString(prevName, prevArtist) + "</b>";
 				}else{
-					document.getElementById("previous").innerHTML = "<b>" + prevName + "</b>" + "  by " + "<b>" + prevArtist + "</b>";
+					document.getElementById("previous").innerHTML = "<b>" + trimString(prevName, prevArtist) + "</b>";
 				}
 				document.getElementById("lastTwelve").innerHTML = "<b>" + recentAmt + "</b> in the last 12 hours";
 				document.getElementById("albumArt").src = lastAlbumArt;
 			});	
 		});			
+		}
 	});				
 }
